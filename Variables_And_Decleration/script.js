b6298838f7d4f7e983793a1f384a330083447215
QuantumCoder <157535720+HanzlaCoding@users.noreply.github.com>
2025-11-09 20:35:25 +0000

    Add initial script.js file with variable declaration examples and explanations
    - Introduced concepts of var, let, and const
    - Included comments on scope, hoisting, and temporal deadzone
    - Provided examples of variable declaration, initialization, reassignment, and redeclaration

---
+++ Variables_And_Decleration/script.js
@@ -0,0 +1,45 @@
+// Variables & Decleration
+// var , let & const
+
+// a = 12;
+// var a = 10;
+// let b = 10;
+// const c = 20;
+
+console.log(a);
+
+var a; // Decleration
+var a = 22; // Decleration & Initialization
+
+// window mein add hota ha
+// function scoped hota ha
+// again declare kr skt ehn or error nhi ayega
+
+
+// Scope global, block , functional
+// var a = 12; //global scoped
+// {var a = 12} // ❌ block scoped ✅ function scoped
+// function(){var = 12} ✅ function scoped
+
+// let (global, functional and block scoped)
+
+// Reassignment & Redecleration
+// var a = 12 //Redecleration
+// a = 14; //reassignment
+
+
+// Temporal Deadzone
+// 🔰
+// 🔰
+// 🔰
+// console.log(a);
+// let a = 12
+
+
+// Hoisting per data types => hoistin gk matlab yeke variable ka initialization wala part upar ajat ha or decleration wala part nichay reh jata ha isi liay var mein hoisting hoti ha or let const mein technically hoti ha or visually nhi hoti.
+/*
+console.log(a) 
+var = 12 ✅ Hoist
+let ❌  
+const ❌  
+*/
